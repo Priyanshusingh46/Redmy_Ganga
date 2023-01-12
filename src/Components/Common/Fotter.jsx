fad4bd328ab9e739deca14b88ea186c6d0ba75b2
Priyanshu <priyanshusingh1877@gmail.com>
2023-01-12 13:38:12 +0530

    edit9

--- src/Components/Common/Fotter.jsx
+++ src/Components/Common/Fotter.jsx
@@ -28,8 +28,9 @@ function Fotter(){
                 <h1 id ="headingFotter">Social Media</h1>
 
                 <div className='socialImageContainer'>
-
+                <a href="https://wa.me/9997483438"class="whatsapp_float"target="_blank"rel="noopener noreferrer">
                     <img id="imagesocial" src={whatsapp} alt="error" />
+                </a>
                     <a href="https://www.instagram.com/uniyalmahendra/">
                     <img id="imagesocial" src={Instagram} alt="error" />
                     </a>
